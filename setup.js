980e7f529d5464113eb362fcf7fa0c2ac3fea6fe
Bill Mills <mills.wj@gmail.com>
2013-10-17 17:45:52 -0700

    many things

--- setup.js
+++ setup.js
@@ -86,9 +86,16 @@ function setup(){
 	//default summing to per clover:
 	document.getElementById('summingScheme').value = 'clover';
 
+	//default x axis to log
+	document.getElementById('xScale').value = 'log';
+	document.getElementById('xScale').onchange = chooseGraphs.bind(null);
+
 	//button setup//////////////////////////////////////////////
     document.getElementById('wikiLink').onclick = function(){
         window.location = 'https://www.triumf.info/wiki/tigwiki/index.php/GRIFFIN_User%27s_Web_Toolkit';
+    };
+    document.getElementById('yieldDB').onclick = function(){
+        window.location = 'http://mis.triumf.ca/science/planning/yield/beam';
     };
 
 	//set up singles efficiency widget//////////////////////////
@@ -119,7 +126,7 @@ function setup(){
 
 	//default to on for demo:
 	HPGeSwitch.onclick();
-	LaBr3Switch.onclick();
+	//LaBr3Switch.onclick();
 	//LEPSSwitch.onclick();
 }
 
@@ -176,10 +183,25 @@ function chooseGraphs(){
 		titles = [],
 		colors = [],
 		min = parseFloat(document.getElementById('xMin').value),
-		max = parseFloat(document.getElementById('xMax').value);
+		max = parseFloat(document.getElementById('xMax').value),
+		HPGeCoef = {},
+		HPGeMinCoef = {},
+		HPGeMaxCoef = {},
+		requestString, i;
+
+	//HPGeCoef holds the coefficients for all HPGe fits in a key value store
+	//where the key is the string concatenation of all the control panel values in 
+	//the order:
+	//summing scheme + nDetectors + HPGe Distance + Delrin thickness
+	//so 'clover1214.520' is 12 detectors at 14.5cm with 20mm delrin and per-clover summing.
+	HPGeCoef['clover811.00'] = [-8.0309801002962706e+02, 1.1187177193972436e+03, -6.7413427087340233e+02, 2.2869743662093572e+02, -4.7716717011091916e+01, 6.2669769394841666e+00, -5.0603869584892447e-01, 2.2979902406689505e-02, -4.4966824986778788e-04];
+	//HPGeMinCoef and HPGeMaxCoef are packed the same way as HPGeCoef, but hold the 1-sigma extrema for the coefficients:
+	HPGeMinCoef['dummy'] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	HPGeMaxCoef['dummy'] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 	if(document.getElementById('enableHPGe').enabled){
-		funcs[funcs.length] = efficient;
+		requestString = 'clover811.00';
+		funcs[funcs.length] = HPGeEfficiency.bind(null, HPGeCoef[requestString], HPGeMinCoef['dummy'], HPGeMaxCoef['dummy']);
 		titles[titles.length] = 'HPGe';
 		colors[colors.length] = '#449944';
 	}
@@ -200,9 +222,11 @@ function chooseGraphs(){
 
 //deploy graphs of [func]tions with [titles]
 function deployGraph(func, titles, colors, min, max){
-	var i, j, x,
+	var i, j, logx, deltaLow, deltaHigh, eff,
 		data = 'Energy[keV]',
-		nPoints = 10000;
+		nPoints = 1000,
+		scaleSelect = document.getElementById("xScale"),
+	    scale = scaleSelect.options[scaleSelect.selectedIndex].value;
 
 
 	for(i=0; i<titles.length; i++){
@@ -211,10 +235,18 @@ function deployGraph(func, titles, colors, min, max){
 	data += '\n';
 
 	for(i=0; i<nPoints; i++){
-			x = parseFloat(((max-min)/nPoints*i).toFixed(2));
-			data += x;
+			if(scale=='lin'){
+				logx = parseFloat( ( (max-min)/nPoints*i+min ).toFixed(2) );
+				data += logx;
+				logx = Math.log(logx);
+			} else{
+				logx = parseFloat( ( (Math.log(max)-Math.log(min))/nPoints*i+Math.log(min) ).toFixed(2) );
+				data += logx;
+			}
 			for(j=0; j<func.length; j++){
-				data += ','+func[j].bind(null, x)();
+				data+=',';
+				eff = func[j].bind(null, logx)();
+				data += eff;
 			}
 			data += '\n';
 	}
@@ -229,11 +261,23 @@ function deployGraph(func, titles, colors, min, max){
 		highlightCircleSize: 6,
 		labelsSeparateLines : true,
 		clickCallback : passClickToWidget,
-		legend: 'always'
+		legend: 'always',
+		customBars: true,
+		axes:{
+			x: {
+				axisLabelFormatter: function(number, gran, opts, dygraph){
+					var val = Math.round(Math.exp(number));
+					if(scale=='lin')
+						return Math.round(number);
+					else
+						return Math.round(Math.exp(number));
+				}
+			}
+		}
 	});
 
 	g.updateOptions({
-		drawCallback: repaint
+		drawCallback: repaint,
 	});
 
 	repaint(g);
@@ -245,12 +289,19 @@ function repaint(dygraph){
 	var xMin = document.getElementById('xMin'),
 		xMax = document.getElementById('xMax'),
 		yMin = document.getElementById('yMin'),
-		yMax = document.getElementById('yMax');
+		yMax = document.getElementById('yMax'),
+		scaleSelect = document.getElementById("xScale"),
+	    scale = scaleSelect.options[scaleSelect.selectedIndex].value;
 
 	prepImageSave(dygraph);
 
-	xMin.value = parseFloat(g.xAxisRange()[0].toFixed(2));
-	xMax.value = parseFloat(g.xAxisRange()[1].toFixed(2));
+	if(scale=='lin'){
+		xMin.value = parseFloat(g.xAxisRange()[0].toFixed(2));
+		xMax.value = parseFloat(g.xAxisRange()[1].toFixed(2));
+	}else{
+		xMin.value = Math.exp(parseFloat(g.xAxisRange()[0].toFixed(2)));
+		xMax.value = Math.exp(parseFloat(g.xAxisRange()[1].toFixed(2)));
+	}
 	yMin.value = parseFloat(g.yAxisRange()[0].toFixed(2));
 	yMax.value = parseFloat(g.yAxisRange()[1].toFixed(2));
 
@@ -378,6 +429,29 @@ function sciNot(val, sig){
 }
 
 //functions//////////////////////////////////////////////////////////////////////////////////
+
+//an eighth order polynomial for logEff in logE with coef. param = [0th order, 1st order, ..., 8th order].
+//lo and hiParam are the 1-sigma extremes of the parameters
+//returns string formatted for inclusion in dygraphs customGraph object.
+function HPGeEfficiency(param, loParam, hiParam, logE){
+	var i,
+		logEff = 0,
+		loDelta = 0,
+		hiDelta = 0,
+		eff;
+
+	for(i=0; i<9; i++){
+		logEff += param[i]*Math.pow(logE,i);
+		//loDelta += Math.pow((param[i] - loParam[i])*Math.pow(logE,i), 2);
+		//hiDelta += Math.pow((hiParam[i] - param[i])*Math.pow(logE,i), 2);
+	}
+	//loDelta = Math.sqrt(loDelta);  //leave these errors out until we have a better grasp of what they should be.
+	//hiDelta = Math.sqrt(hiDelta);
+
+	eff = Math.exp(logEff);
+	return (eff - eff*loDelta) + ';' + eff + ';' + (eff + eff*hiDelta);
+}
+
 function efficient(x){
 	var f = Math.exp(-(x-2)*(x-2)/4);
 	return f;
@@ -397,3 +471,131 @@ function exponent(x){
 	var f = Math.pow(2, -x);
 	return f
 }
+
+/////Dygraph hax/////////////////////////////////////////////////////////////
+
+//hack to cope with the questionable decision to not support logarithmic x-axes:
+Dygraph.numericTicks = function(a, b, pixels, opts, dygraph, vals) {
+  var pixels_per_tick = /** @type{number} */(opts('pixelsPerLabel'));
+  var ticks = [];
+  var i, j, tickV, nTicks;
+  if (vals) {
+    for (i = 0; i < vals.length; i++) {
+      ticks.push({v: vals[i]});
+    }
+  } else {
+    // TODO(danvk): factor this log-scale block out into a separate function.
+    if (opts("logscale")) {
+      nTicks  = Math.floor(pixels / pixels_per_tick);
+      var minIdx = Dygraph.binarySearch(a, Dygraph.PREFERRED_LOG_TICK_VALUES, 1);
+      var maxIdx = Dygraph.binarySearch(b, Dygraph.PREFERRED_LOG_TICK_VALUES, -1);
+      if (minIdx == -1) {
+        minIdx = 0;
+      }
+      if (maxIdx == -1) {
+        maxIdx = Dygraph.PREFERRED_LOG_TICK_VALUES.length - 1;
+      }
+      // Count the number of tick values would appear, if we can get at least
+      // nTicks / 4 accept them.
+      var lastDisplayed = null;
+      if (maxIdx - minIdx >= nTicks / 4) {
+        for (var idx = maxIdx; idx >= minIdx; idx--) {
+          var tickValue = Dygraph.PREFERRED_LOG_TICK_VALUES[idx];
+          var pixel_coord = Math.log(tickValue / a) / Math.log(b / a) * pixels;
+          var tick = { v: tickValue };
+          if (lastDisplayed === null) {
+            lastDisplayed = {
+              tickValue : tickValue,
+              pixel_coord : pixel_coord
+            };
+          } else {
+            if (Math.abs(pixel_coord - lastDisplayed.pixel_coord) >= pixels_per_tick) {
+              lastDisplayed = {
+                tickValue : tickValue,
+                pixel_coord : pixel_coord
+              };
+            } else {
+              tick.label = "";
+            }
+          }
+          ticks.push(tick);
+        }
+        // Since we went in backwards order.
+        ticks.reverse();
+      }
+    }
+
+    // ticks.length won't be 0 if the log scale function finds values to insert.
+    if (ticks.length === 0) {
+      // Basic idea:
+      // Try labels every 1, 2, 5, 10, 20, 50, 100, etc.
+      // Calculate the resulting tick spacing (i.e. this.height_ / nTicks).
+      // The first spacing greater than pixelsPerYLabel is what we use.
+      // TODO(danvk): version that works on a log scale.
+      var kmg2 = opts("labelsKMG2");
+      var mults, base;
+      if (kmg2) {
+        mults = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+        base = 16;
+      } else {
+        mults = [1, 2, 5, 10, 20, 50, 100];
+        base = 10;
+      }
+      //hack
+	  var scaleSelect = document.getElementById("xScale"),
+	      scale = scaleSelect.options[scaleSelect.selectedIndex].value;
+	  if(scale == 'log'){
+	      mults = [1*Math.log(10), 2*Math.log(10), 5*Math.log(10), 10*Math.log(10), 20*Math.log(10), 50*Math.log(10), 100*Math.log(10)]//[Math.log(10)]
+    	  base = 10
+      }
+      //end hack
+
+      // Get the maximum number of permitted ticks based on the
+      // graph's pixel size and pixels_per_tick setting.
+      var max_ticks = Math.ceil(pixels / pixels_per_tick);
+
+      // Now calculate the data unit equivalent of this tick spacing.
+      // Use abs() since graphs may have a reversed Y axis.
+      var units_per_tick = Math.abs(b - a) / max_ticks;
+
+      // Based on this, get a starting scale which is the largest
+      // integer power of the chosen base (10 or 16) that still remains
+      // below the requested pixels_per_tick spacing.
+      var base_power = Math.floor(Math.log(units_per_tick) / Math.log(base));
+      var base_scale = Math.pow(base, base_power);
+
+      // Now try multiples of the starting scale until we find one
+      // that results in tick marks spaced sufficiently far apart.
+      // The "mults" array should cover the range 1 .. base^2 to
+      // adjust for rounding and edge effects.
+      var scale, low_val, high_val, spacing;
+      for (j = 0; j < mults.length; j++) {
+        scale = base_scale * mults[j];
+        low_val = Math.floor(a / scale) * scale;
+        high_val = Math.ceil(b / scale) * scale;
+        nTicks = Math.abs(high_val - low_val) / scale;
+        spacing = pixels / nTicks;
+        if (spacing > pixels_per_tick) break;
+      }
+
+      // Construct the set of ticks.
+      // Allow reverse y-axis if it's explicitly requested.
+      if (low_val > high_val) scale *= -1;
+      for (i = 0; i <= nTicks; i++) {
+        tickV = low_val + i * scale;
+        ticks.push( {v: tickV} );
+      }
+    }
+  }
+
+  var formatter = /**@type{AxisLabelFormatter}*/(opts('axisLabelFormatter'));
+
+  // Add labels to the ticks.
+  for (i = 0; i < ticks.length; i++) {
+    if (ticks[i].label !== undefined) continue;  // Use current label.
+    // TODO(danvk): set granularity to something appropriate here.
+    ticks[i].label = formatter(ticks[i].v, 0, opts, dygraph);
+  }
+
+  return ticks;
+};
